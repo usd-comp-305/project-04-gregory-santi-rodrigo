01402b16fee216ed5c8ad112e95f2c4940f9ae69
Gregory Monzon <gmonzon@sandiego.edu>
2026-04-28 21:30:19 -0700

    added FineDiningRestaurant.java stub

---
+++ src/main/java/edu.sandiego.comp305/FineDiningRestaurant.java
@@ -0,0 +1,19 @@
+package edu.sandiego.comp305;
+
+public class FineDiningRestaurant extends AbstractRestaurant{
+
+    private static final int OPEN_HOUR = 17;
+    private static final int CLOSE_HOUR = 23;
+    private static final int DEFAULT_HAPPY_HOUR = 17;
+    private static final int BASE_MAX_ORDERS = 40;
+
+    public FineDiningRestaurant(){
+        super("Fine Dining", RestaurantType.FINE_DINING,
+                OPEN_HOUR, CLOSE_HOUR, DEFAULT_HAPPY_HOUR, BASE_MAX_ORDERS);
+    }
+
+    @Override
+    public double processOrder(Order order){
+        return processOrderBase(order);
+    }
+}
