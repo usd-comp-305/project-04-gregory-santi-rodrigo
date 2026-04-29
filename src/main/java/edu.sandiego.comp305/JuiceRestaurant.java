71ed304f26cf3f96cb0f413d8b9d03ff904ae9eb
Gregory Monzon <gmonzon@sandiego.edu>
2026-04-28 21:37:08 -0700

    added JuiceRestaurant.java stub

---
+++ src/main/java/edu.sandiego.comp305/JuiceRestaurant.java
@@ -0,0 +1,19 @@
+package edu.sandiego.comp305;
+
+public class JuiceRestaurant extends AbstractRestaurant{
+
+    private static final int OPEN_HOUR = 8;
+    private static final int CLOSE_HOUR = 19;
+    private static final int DEFAULT_HAPPY_HOUR = 8;
+    private static final int BASE_MAX_ORDERS = 60;
+
+    public JuiceRestaurant(){
+        super("Juice Bar", RestaurantType.JUICE_BAR,
+                OPEN_HOUR, CLOSE_HOUR, DEFAULT_HAPPY_HOUR, BASE_MAX_ORDERS);
+    }
+
+    @Override
+    public double processOrder(Order order){
+        return processOrderBase(order);
+    }
+}
