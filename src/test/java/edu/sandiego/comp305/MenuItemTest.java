8f28edaa378a7008413d3c0f24c19caf8b31fac3
Gregory Monzon <gmonzon@sandiego.edu>
2026-05-06 21:27:49 -0700

    feat: created a test that ensures all  menu items have correct price

--- src/test/java/edu/sandiego/comp305/MenuItemTest.java
+++ src/test/java/edu/sandiego/comp305/MenuItemTest.java
@@ -62,6 +62,10 @@ public class MenuItemTest {
     }
 
     @Test
+    public void testFineDiningReturnsCorrectPrice(){
+    }
+
+    @Test
     public void testJuiceBarReturnsCorrectName(){
         assertEquals("Orange Juice", JuiceBarMenuItem.ORANGE_JUICE.getName(),
                 "ORANGE_JUICE should return its display name.");
