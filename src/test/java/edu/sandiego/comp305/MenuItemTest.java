a1ac9cf9f0295cb1d8f687722d29b7118cb9d505
Gregory Monzon <gmonzon@sandiego.edu>
2026-05-06 18:41:28 -0700

    feat: added test to make sure juice bar's menu items have correct names

--- src/test/java/edu/sandiego/comp305/MenuItemTest.java
+++ src/test/java/edu/sandiego/comp305/MenuItemTest.java
@@ -1,4 +1,18 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MenuItemTest {
+
+    @Test
+    public void testJuiceBarReturnsCorrectName(){
+        assertEquals("Orange Juice", JuiceBarMenuItem.ORANGE_JUICE.getName(),
+                "ORANGE_JUICE should return its display name.");
+        assertEquals("Strawberry Smoothie", JuiceBarMenuItem.STRAWBERRY_SMOOTHIE.getName());
+        assertEquals("Acai Bowl", JuiceBarMenuItem.ACAI_BOWL.getName());
+        assertEquals("Ginger Shot", JuiceBarMenuItem.GINGER_SHOT.getName());
+    }
 }
