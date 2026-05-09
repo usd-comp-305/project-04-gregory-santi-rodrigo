02b1690001447e11ff474f0d1c4906020fa53dbb
Gregory Monzon <gmonzon@sandiego.edu>
2026-05-09 16:11:26 -0700

    feat: added test stub that has a profit and loss when different restaurants gain and lose money

--- src/test/java/edu/sandiego/comp305/DailyReportTest.java
+++ src/test/java/edu/sandiego/comp305/DailyReportTest.java
@@ -53,4 +53,9 @@ public class DailyReportTest {
     public void testGetTotalNetChangeIsNegativeWhenRestaurantsLoseMoney(){
 
     }
+
+    @Test
+    public void testGetTotalNetChangeWithMixedProfitAndLoss(){
+
+    }
 }
