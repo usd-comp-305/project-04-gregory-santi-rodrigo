922483c511fdc5e1ba184424d36d3f01f3456245
Gregory Monzon <gmonzon@sandiego.edu>
2026-05-09 14:42:19 -0700

    feat: added test stub that returns correct restaurant name

--- src/test/java/edu/sandiego/comp305/RestaurantReportTest.java
+++ src/test/java/edu/sandiego/comp305/RestaurantReportTest.java
@@ -1,4 +1,24 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RestaurantReportTest {
+
+    private static final String TEST_NAME = "BBQ Shack";
+
+    private static final double REGULAR_REVENUE = 100.00;
+
+    private static final double HAPPY_REVENUE = 40.00;
+
+    private static final int PEAK_HOUR = 14;
+
+    private RestaurantReport buildReport(){
+        return new RestaurantReport(TEST_NAME, REGULAR_REVENUE, HAPPY_REVENUE, PEAK_HOUR);
+    }
+
+    @Test
+    public void testGetRestaurantNameReturnsCorrectName(){
+
+    }
 }
