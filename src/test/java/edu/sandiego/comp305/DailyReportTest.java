eca95d784a0ba33d2ebac2f99e770ef098697270
Gregory Monzon <gmonzon@sandiego.edu>
2026-05-09 16:06:34 -0700

    feat: added test stub that has getRestaurantReport() return the an empty report with no restaurants

--- src/test/java/edu/sandiego/comp305/DailyReportTest.java
+++ src/test/java/edu/sandiego/comp305/DailyReportTest.java
@@ -28,4 +28,9 @@ public class DailyReportTest {
     public void testGetRestaurantReportsReturnsCorrectSize(){
 
     }
+
+    @Test
+    public void testGetRestaurantReportsIsEmptyWithNoRestaurants(){
+
+    }
 }
