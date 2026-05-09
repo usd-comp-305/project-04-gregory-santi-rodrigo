0ff94dd38bb484e26f47c2b439453c848b60dd3a
Gregory Monzon <gmonzon@sandiego.edu>
2026-05-09 14:42:59 -0700

    feat: added test stub that returns regular revenue passed in

--- src/test/java/edu/sandiego/comp305/RestaurantReportTest.java
+++ src/test/java/edu/sandiego/comp305/RestaurantReportTest.java
@@ -21,4 +21,9 @@ public class RestaurantReportTest {
     public void testGetRestaurantNameReturnsCorrectName(){
 
     }
+
+    @Test
+    public void testGetRegularRevenueReturnsCorrectValue(){
+
+    }
 }
