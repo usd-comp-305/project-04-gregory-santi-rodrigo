65f4f8b90dc0b4243aacc511ed424ec619509b82
Rodrigo <rgarrido@sandiego.edu>
2026-05-10 19:20:04 -0700

    Added failing test for Order.getItem()

--- src/test/java/edu/sandiego/comp305/OrderTest.java
+++ src/test/java/edu/sandiego/comp305/OrderTest.java
@@ -1,4 +1,23 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class OrderTest {
+    private Order order;
+    private final int TEST_HOUR = 12;
+    private final RestaurantType TEST_TYPE = RestaurantType.BURGER;
+    private final MenuItem TEST_ITEM = BurgerMenuItem.CHEESEBURGER;
+
+    @BeforeEach
+    void init() {
+        order = new Order(TEST_ITEM, TEST_HOUR, TEST_TYPE);
+    }
+
+    @Test
+    public void getItem_ReturnInitialItem() {
+        assertEquals(TEST_ITEM, order.getItem(), "The returned item should match the input");
+    }
 }
