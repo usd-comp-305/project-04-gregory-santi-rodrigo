fd936059350b18013ac313a78eab3685ef3c3c24
Rodrigo <rgarrido@sandiego.edu>
2026-05-10 21:43:59 -0700

    Added failing test for isOpen during operation hours

--- src/test/java/edu/sandiego/comp305/RestaurantTest.java
+++ src/test/java/edu/sandiego/comp305/RestaurantTest.java
@@ -1,4 +1,24 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RestaurantTest {
+
+    private BurgerRestaurant restaurant;
+    private final MenuItem TEST_ITEM = BurgerMenuItem.CHEESEBURGER;
+
+    @BeforeEach
+    void init() {
+        restaurant = new BurgerRestaurant();
+    }
+
+    @Test
+    public void isOpen_ReturnTrueDuringOperatingHours() {
+        assertTrue(restaurant.isOpen(11), "Should be open at the exact opening hour (11)");
+        assertTrue(restaurant.isOpen(15), "Should be open mid-day (15)");
+        assertTrue(restaurant.isOpen(20), "Should be open one hour before closing (20)");
+    }
 }
