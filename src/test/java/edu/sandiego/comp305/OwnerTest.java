aed3746c987ead8dd060b9b3fd4762b03024b210
Gregory Monzon <gmonzon@sandiego.edu>
2026-05-10 17:05:19 -0700

    feat: set up universal variables and @BeforeEach step for OwnerTest.java

--- src/test/java/edu/sandiego/comp305/OwnerTest.java
+++ src/test/java/edu/sandiego/comp305/OwnerTest.java
@@ -1,4 +1,26 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class OwnerTest {
+
+    private static final double STARTING_NET_WORTH = 1000.00;
+
+    private static final double GOAL_NET_WORTH = 5000.00;
+
+    private static final int PEAK_HOUR = 14;
+
+    private Owner testOwner;
+    private BBQRestaurant testRestaurant;
+
+    @BeforeEach
+    public void setUp(){
+        testRestaurant = new BBQRestaurant();
+        testOwner = new Owner(STARTING_NET_WORTH, GOAL_NET_WORTH,
+                new ArrayList<>(List.of(testRestaurant)));
+    }
 }
